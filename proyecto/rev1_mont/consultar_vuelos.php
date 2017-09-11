2fe69b168bdb251fe2aa8a2bc28c53d172850471
Carlos <montiel@galileo.edu>
2017-09-10 22:43:06 -0600

    Rev1

---
+++ proyecto/rev1_mont/consultar_vuelos.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+     <title>
+        Consulta de vuelo
+     </title>
+  </head>
+  <body>
+
+
+     <center>
+         <a href="index.html">regresar</a>
+     </center>
+  </body>
+</html>
